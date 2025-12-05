7e82742da00d191d9e3252d1f22f11cadc0f97bc
Karl Hans Laurits <karl@laurits.dev>
2025-12-05 21:43:57 +0200

    refactor: make __init__.py files more consistent

--- src/budy/views/__init__.py
+++ src/budy/views/__init__.py
@@ -1,16 +1,16 @@
-from .budget_list import render_budget_list as render_budget_list
-from .budget_status import render_budget_status as render_budget_status
-from .messages import (
-    render_error as render_error,
-)
-from .messages import (
-    render_success as render_success,
-)
-from .messages import (
-    render_warning as render_warning,
-)
-from .transaction_list import (
-    render_simple_transaction_list as render_simple_transaction_list,
-)
-from .transaction_list import render_transaction_list as render_transaction_list
-from .payee_ranking import render_payee_ranking as render_payee_ranking
+from .budget_list import render_budget_list
+from .budget_status import render_budget_status
+from .messages import render_error, render_success, render_warning
+from .payee_ranking import render_payee_ranking
+from .transaction_list import render_simple_transaction_list, render_transaction_list
+
+__all__ = [
+    "render_error",
+    "render_warning",
+    "render_success",
+    "render_transaction_list",
+    "render_simple_transaction_list",
+    "render_payee_ranking",
+    "render_budget_list",
+    "render_budget_status",
+]
